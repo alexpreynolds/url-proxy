ead399121dc10b261c2956f9d2109474950fc482
Alex Reynolds <alexpreynolds@gmail.com>
2019-07-29 19:09:57 +0000

    disabled caching and added request limits/checks

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # url-proxy
 
-This is an Expressjs application that takes an URL-encoded web address as an argument ("URL"), requests that URL via an HTTP GET method, and writes the response as a UTF-8-encoded (text) response back to the requesting client.
+A small and simple expressjs application that takes an URL-encoded web address as an argument, performs a GET request for that URL, and writes the response as a UTF8-encoded text response back to the requesting client.
+
+To limit usage to our purposes, the server limits the response to *100 kilobytes* (or the value of the `BYTELIMIT` environment variable, if defined). 
+
+If the response contains no `content-length` header, we immediately return a 411 error. If the requested file has a content length that is larger than the size limit, we send a 400 response immediately. 
+
+We could also put a limit on the number of lines in the file, but I decided to disable this check. The client (in this case, the calling web application using this proxy service) can put its own limit on the total number of lines used from the response.
+
+To limit troubleshooting problems, we disable caching and also always return UTF8-encoded text.
--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const https = require('https');
 const fs = require('fs');
 const debug = require('debug')('url-proxy:server');
 const normalizePort = require('normalize-port');
+const nocache = require('nocache');
+const morgan = require('morgan');
 
 /**
  * Listen
@@ -20,18 +22,21 @@ const normalizePort = require('normalize-port');
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '9001');
+let port = normalizePort(process.env.PORT || '9001');
 app.set('port', port);
 
-var privateKey = fs.readFileSync('/etc/ssl/private/altius.org.key');
-var certificate = fs.readFileSync('/etc/ssl/certs/altius-bundle.crt');
+let byteLimit = (process.env.BYTELIMIT || 1024*1024);
+// let lineLimit = (process.env.LINELIMIT || 100);
+
+let privateKey = fs.readFileSync('/etc/ssl/private/altius.org.key');
+let certificate = fs.readFileSync('/etc/ssl/certs/altius-bundle.crt');
 
 const options = {
   key: privateKey,
   cert: certificate
 };
 
-var server = https.createServer(options, app);
+let server = https.createServer(options, app);
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
@@ -45,7 +50,7 @@ function onError(error) {
     throw error;
   }
 
-  var bind = typeof port === 'string'
+  let bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port;
 
@@ -69,11 +74,20 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
+  let addr = server.address();
+  let bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
+}
+
+/**
+ * Response must be text
+ */
+ 
+function textonly(req, res, next) {
+  res.set('Content-Type', 'text/plain');
+  return next();
 }
 
 /**
@@ -81,14 +95,13 @@ function onListening() {
  */
 
 function cors(req, res, next) {
-  res.set('Content-Type', 'text/plain');
   res.set('Access-Control-Allow-Origin', req.headers.origin);
   res.set('Access-Control-Allow-Methods', req.method);
   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
   res.set('Access-Control-Allow-Credentials', true);
 
   // Respond OK if the method is OPTIONS
-  if(req.method === 'OPTIONS') {
+  if (req.method === 'OPTIONS') {
     return res.send(200);
   } else {
     return next();
@@ -96,16 +109,42 @@ function cors(req, res, next) {
 }
 
 /**
- * CORS
+ * Response, CORS, cache policy and logging
  */
 
+app.use(textonly);
 app.use(cors);
+app.use(nocache());
+app.use(morgan('combined'));
 
 /**
  * /:url (url must be URL-encoded)
  */
 
 app.all('/:url', (req, res, next) => {
-  var url = req.params.url;
-  req.pipe(request(url)).pipe(res);
+  let url = req.params.url;
+  let lineCount = -1;
+  /**
+   * Short-circuit the final response when limits are violated or the URL-response is malformed
+   */
+  req
+    .pipe(request(url))
+    .on('response', function(response) {
+      if (!response.headers['content-length']) {
+        res.status(411).send("Content length header required");
+      }
+      let contentLength = parseInt(response.headers['content-length']);
+      if (contentLength > byteLimit) {
+        res.status(400).send("Went over byte limit");
+      }
+    })
+/*
+    .on('data', function(data) {
+      lineCount += data.toString('utf8').split(/\r\n|\r|\n/).length;
+      if (lineCount > lineLimit) {
+        res.status(400).send("Went over line limit");
+      }
+    })
+*/
+    .pipe(res);
 });
